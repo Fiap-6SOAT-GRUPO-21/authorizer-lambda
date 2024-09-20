0981bd79b9897d0c9126d839ba7b2df76cb2e41e
JoseVitorDurante <josevitor_limma@hotmail.com>
2024-09-20 14:23:15 -0300

    commit inicial

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import {CognitoJwtVerifier} from "aws-jwt-verify";
 
 const verifier = CognitoJwtVerifier.create({
-    // userPoolId: process.env.USER_POOL_ID, todo: change this to the user pool id of the user pool
-    userPoolId: "us-east-1_EXlHzpLBS",
+    userPoolId: process.env.USER_POOL_ID,
     tokenUse: "access",
-    // clientId: process.env.CLIENT_ID, todo: change this to the client id of the app client
-    clientId: "6d1oc64ts7urk81mcaltc3ip68",
+    clientId: process.env.CLIENT_ID,
 });
 
 export const handler = async (event) => {
